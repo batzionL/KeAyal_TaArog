e0ae65042eddc193d3b97c97a4e62fe8a712656e
batzio <aniandata1234@gmail.com>
2025-02-06 08:19:44 +0200

    Add service and controller files

---
+++ Backend/controller.py
@@ -0,0 +1,172 @@
+from flask import request, jsonify
+from main import app
+from service import add_patient_to_db, add_treatment_to_db, add_event_to_db, get_patient_from_db, get_patient_by_id_from_db, get_owner_from_db, get_patient_treatments_from_db, get_all_events_from_db, update_event_in_db
+    
+@app.route("/add_patient", methods=["POST"])
+def add_patient():
+    try:
+        data = request.json
+        answer = add_patient_to_db(data)
+        if answer:
+            return jsonify({"message": "atient added successfully!"}), 200
+        else:
+            return jsonify({"message": "Could not add this patient"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+
+@app.route("/add_treatment", methods=["POST"])
+def add_treatment():
+    try:
+        data = request.json
+        answer = add_treatment_to_db(data)
+        if "error" in answer:
+            if answer["error"] == "Patient not found":
+                return jsonify({"error": answer["error"]}), 404
+            elif answer["error"] == "Invalid date format":
+                return jsonify({"error": answer["error"]}), 400
+            else:
+                return jsonify({"error": answer["error"]}), 500
+        return jsonify({"message": "Treatment added successfully!"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+
+@app.route("/add_event", methods=["POST"])
+def add_event():
+    try:
+        data = request.json
+        answer = add_event_to_db(data)
+        if answer:
+            return jsonify({"message": "Event was added successfully!"}), 201
+        else:
+            return jsonify({"message": "Could not add this event!"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@app.route("/get_patient", methods=["POST"])
+def validate_patient():
+    try:
+        data = request.json
+        patient_id = data.get("patientId")
+        password = data.get("password")
+        answer = get_patient_from_db(patient_id, password)
+        if answer:
+            return jsonify({"firstName": answer.firstName}), 201
+        else:
+            return jsonify({"error": "Patient not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+    
+@app.route("/get_patient_id", methods=["POST"])
+def get_patient():
+  try:
+      data = request.json
+      patient_id = data.get("id")
+      answer = get_patient_by_id_from_db(patient_id)
+      if answer:
+          return jsonify({"firstName": answer.firstName}), 201
+      else:
+          return jsonify({"error": "Patient not found"}), 404
+  except Exception as e:
+      return jsonify({"error": str(e)}), 500
+  
+  
+@app.route("/get_owner", methods=["POST"])
+def get_owner():
+    try:
+        data = request.json
+        patient_id = data.get("patientId")
+        answer = get_owner_from_db(patient_id)
+        if answer:
+            return jsonify({"message": "Owner found successfully"}), 201
+        else:
+            return jsonify({"error": "Owner not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@app.route("/get_patient_details", methods=["POST"])
+def get_patient_details():
+    try:
+        data = request.json
+        patient_id = data.get("patientId")
+        answer = get_patient_by_id_from_db(patient_id)
+        if answer:
+            return jsonify({"firstName": answer.firstName, "motherName": answer.motherName, "age":answer.dateOfBirth}), 201
+        else:
+            return jsonify({"error": "Patient not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+
+@app.route("/get_patient_treatments", methods=["POST"])
+def get_patient_treatments():
+    try:
+        data = request.json
+        id = data.get("id")
+        answer = get_patient_treatments_from_db(id)
+        if answer:
+            treatments_list = [
+                {
+                    "treatment_date": treatment.treatment_date,
+                    "problem": treatment.problem,
+                    "TherapeuticProcess": treatment.TherapeuticProcess,
+                    "homeWork": treatment.homeWork,
+                }
+                for treatment in answer
+            ]
+            return jsonify(treatments_list), 200
+        else:
+            return jsonify({"error": "Treatment not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@app.route("/get_all_events", methods=["GET"])
+def get_all_events():
+    try:
+        answer = get_all_events_from_db()
+        if answer:
+            events_list = [
+                {
+                    "eventDate": event.eventDate,
+                    "eventTime": event.eventTime,
+                    "freeOrBusy": event.freeOrBusy
+                }
+                for event in answer
+            ]
+            return jsonify(events_list), 201
+        else:
+            return jsonify({"error": "Events not found"}), 404
+    except Exception as e:
+        return jsonify({"Error: ": str(e)}), 500
+    
+
+
+@app.route("/update_event", methods=["PUT"])
+def update_event():
+    try:
+        data = request.json
+        eventDate = data.get("eventDate")
+        eventTime = data.get("eventTime")
+        freeOrBusy = data.get("freeOrBusy")
+        patientId = data.get("id")
+        
+        if not all([eventDate, eventTime, freeOrBusy, patientId]):
+            return jsonify({"error": "Missing required fields"}), 400
+        answer = update_event_in_db(eventDate=eventDate, eventTime=eventTime)
+        if not answer:
+            return jsonify({"error": "Event not found"}), 404
+        answer.update(
+            set__freeOrBusy=freeOrBusy,
+            set__patientId=patientId
+        )
+        return jsonify({"message": "Event updated successfully"}), 200
+    except Exception as e:
+        print(f"Unexpected error: {str(e)}")
+        return jsonify({"error": str(e)}), 500
---
+++ Backend/service.py
@@ -0,0 +1,117 @@
+from datetime import datetime
+from models.patient import Patient
+from models.treatment import Treatment
+from models.owner import Owner
+from models.event import Event
+
+
+# 
+def add_patient_to_db(data):
+    try:
+        new_patient = Patient(
+        firstName=data["firstName"],
+        lastName=data["lastName"],
+        patientId=data["patientId"],
+        email=data["email"],
+        password=data["password"],
+        phone=data["phone"],
+        motherName=data["motherName"],
+        dateOfBirth=data["dateOfBirth"]
+        )
+        new_patient.save()
+        return new_patient
+    except Exception as e:
+        raise Exception(f"Error fetching patient: {str(e)}")
+    
+
+# 
+def add_treatment_to_db(data):
+    try:
+        patient_id = data.get("patientId")
+        patient = Patient.objects(patientId=patient_id).first()
+        if not patient:
+            return {"error": "Patient not found"}
+        try:
+            treatment_date = datetime.fromisoformat(data["treatment_date"].replace("Z", "+00:00"))
+        except ValueError:
+            return {"error": "Invalid date format"}
+        
+        new_treatment = Treatment(
+            patientId=data["patientId"],
+            treatment_date=treatment_date,
+            problem=data["problem"],
+            TherapeuticProcess=data["TherapeuticProcess"],
+            homeWork=data["homeWork"],
+            patient=patient
+        )
+        new_treatment.save()
+        
+        patient.treatment_ids.append(str(new_treatment.id))
+        patient.save()
+
+        return new_treatment
+    except Exception as e:
+        return {"Error fetching treatment": str(e)}
+
+# 
+def add_event_to_db(data):
+    try:
+        new_event = Event(
+            eventDate=data["newDate"],
+            eventTime=data["newTime"],
+            freeOrBusy=data["freeOrBusy"],
+            patientId="0"
+        )
+        new_event.save()
+        return new_event
+    except Exception as e:
+        return {"Error fetching event": str(e)}
+
+# 
+def get_patient_from_db(patient_id, password):
+    try:
+        patient = Patient.objects(patientId=patient_id, password=password).first()
+        return patient
+    except Exception as e:
+        return {"Error fetching patient": str(e)}
+
+
+def get_patient_by_id_from_db(patient_id):
+    try:
+        patient = Patient.objects(patientId=patient_id).first()
+        return patient
+    except Exception as e:
+        return {"Error fetching patient": str(e)}
+
+
+def get_owner_from_db(id):
+    try:
+        owner = Owner.objects(ownerId=int(id)).first()
+        return owner
+    except Exception as e:
+        return {"Error fetching owner": str(e)}
+
+
+def get_patient_treatments_from_db(id):
+    try:
+        treatments = Treatment.objects(patientId=id)
+        return treatments
+    except Exception as e:
+        return {"Error fetching treatments": str(e)}
+    
+
+def get_all_events_from_db():
+    try:
+        events = Event.objects()
+        return events
+    except Exception as e:
+        return {"Error fetching events": str(e)}
+        
+
+def update_event_in_db(eventDate, eventTime):
+    try:
+        event = Event.objects(eventDate=eventDate, eventTime=eventTime).first()
+        return event
+    except Exception as e:
+        return {"Error update event": str(e)}
+        
