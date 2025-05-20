560a997b8f569f1a9031a0afcf1717669c939aec
batzio <aniandata1234@gmail.com>
2025-05-20 13:38:20 +0300

    Change DB to MongoDB Atlas

--- Backend/main.py
+++ Backend/main.py
@@ -2,13 +2,24 @@ from flask import Flask
 from flask_cors import CORS
 from mongoengine import connect
 from controller import controller_bp 
-
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-connect(db="KeAyaal_TaArog", host="localhost", port=27017)
+dotenv_path = os.path.join(os.path.dirname(__file__), '../Frontend/.env')
+load_dotenv(dotenv_path)
+
+mongo_uri = os.getenv("MONGODB_URI")
+print("MONGO URI:", mongo_uri)
+
+connect(
+    host=mongo_uri
+)
+
 app.register_blueprint(controller_bp)
+
 
 
 if __name__ == "__main__":
